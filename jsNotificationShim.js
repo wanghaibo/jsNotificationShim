50c69310af3c9e9be39cb0315d67cc80ff220e0b
wanghaibo@myhexin.com <wanghaibo@myhexin.com>
2014-04-23 22:35:38 +0800

    update

--- jsNotificationShim.js
+++ jsNotificationShim.js
@@ -1,20 +1,26 @@
-function sendNotification (title, options) {
-    // Memoize based on feature detection.
-    if ("Notification" in window) {
-        sendNotification = function (title, options) {
-            new Notification(title, options);
-        };
-    } else if ("mozNotification" in navigator) {
-        sendNotification = function (title, options) {
-            // Gecko < 22
-            navigator.mozNotification
-            .createNotification(title, options.body, options.icon)
-            .show();
-        };
-    } else {
-        sendNotification = function (title, options) {
-            alert(title + ": " + options.body);
-        };
+//出现多个需要同时notify时，使用for循环火狐只会弹出一个，只能使用递归调用通过Onshow回调实现
+function alertTipWindow(newsList) {
+    var notify  = function(i) {
+        var title   = newsList[i].title;
+        var n   = new Notification(title, {
+            body:'来源:'+newsList[i].mediaName
+        });
+        n.onclick   = function() {
+            window.open(newsList[i].originUrl);
+        }
+        var j = i-1;
+        n.onshow    = function() {
+            var me  = this;
+            setTimeout(function() {
+                me.close();
+            }, 5000);
+            if (j >= 0) {
+                notify(j);
+            }
+        }
     }
-    sendNotification(title, options);
-};
+    if (newsList.length) {
+        notify(newsList.length-1);
+    }
+}
+alertTipWindow([{"title":"ccc","originUrl":"http://0033.com","mediaName":"日本","snatchTime":1398263649},{"title":"aaa","originUrl":"http://www.baidu.com","mediaName":"中国","snatchTime":1398263649},{"title":"bbb","originUrl":"http://www.google.com","mediaName":"美国","snatchTime":1398263649,"alertDisplay":1}]);
